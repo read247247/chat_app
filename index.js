f78495a33a9d51e26a565073850c14197b139634
Nathan Godard <nathan.godard1@ucalgary.ca>
2020-11-13 17:23:40 -0700

    First commit
    Fisrt commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const socket_io = require('socket.io');
+const io = socket_io(http);
+const name_generator = require('random-username-generator');
+const express = require('express');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+let users = [];
+let id = [];
+let num_users = 0;
+let message_history = [];
+
+io.on('connection', (socket) => {
+    if(id.includes(socket.id)) { //Check if user has been here before
+        //Change status to connected
+    }
+    else {
+        let username = name_generator.generate();
+        users.push({
+            "id": socket.id,
+            "username": username,
+            "color": "white",
+            "connected": true
+        });
+        id.push(socket.id);
+        socket.emit('first_connection', {"username": username, "color": "white", "id": socket.id});
+        io.emit('connected_users', users);
+    }
+    num_users++;
+    socket.emit('message_history', message_history);
+    console.log("User " + socket.id + " connected, now " + num_users);
+
+    socket.on('chat message', (msg_in) => {
+        if(msg_in.text.startsWith("/name ")){
+            users.filter(user => {
+                if(user.id === socket.id){
+                    user.username = msg_in.text.substring(6);
+                }
+            });
+            io.emit('connected_users', users);
+            return;
+        }
+        else if(msg_in.text.startsWith("/color ")){
+            users.filter(user => {
+                if(user.id === socket.id){
+                    user.color = msg_in.text.substring(7);
+                }
+            });
+            io.emit('connected_users', users);
+            return;
+        }
+        const date = new Date();
+        let msg_out = {};
+        msg_out.timeStamp = date.getHours() + ":" + date.getMinutes();
+        msg_out.username = msg_in.username;
+        msg_out.text = msg_in.text;
+        msg_out.color = msg_in.color;
+        msg_out.id = msg_in.id;
+        console.log(msg_out.text);
+        msg_out.text = msg_out.text.replace(":)", "😄");
+        msg_out.text = msg_out.text.replace(":(", "🙁");
+        msg_out.text = msg_out.text.replace(":o", "😮");
+        console.log(msg_out.text);
+        io.emit('chat message', msg_out);
+        if(message_history.length >= 200){
+            message_history = message_history.slice();
+        }
+        message_history.push(msg_out);
+    });
+
+    socket.on('disconnect', () => {
+        id = id.filter(item => item !== socket.id);
+        users.filter(user => {
+            if(user.id === socket.id){
+                user.connected = false;
+            }
+        });
+
+        // Figure out which user disconnected and set them to disconnected
+        num_users--;
+        io.emit('connected_users', users);
+        console.log(socket.id + " disconnected");
+        console.log(num_users + " remaining users are " + id);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
